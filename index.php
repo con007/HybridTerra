33ee028ad9cceff3d9f6dc4ba3159b9ca5b0914d
VikasGoswami <vikaskv001@gmail.com>
2020-06-13 02:20:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ welcome to Hybrid Multicloud cloud
 <?php
 
 print `top`;
+echo ' 
+<!DOCTYPE html">
+<html>
+<img src="/var/www/html/image/20191110_075122.jpg" width="280" height="125" title="My Picture" alt="Picture at Uttrakhand!!" />
 
+</html>
+';
 ?>
 </pre>
